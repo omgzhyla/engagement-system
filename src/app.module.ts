9cdd6c9b332f5ac65e65a6c5f7fe0c80abfa2532
Pavlo Zhyla <pavlo@pinpointestimates.com>
2022-07-30 14:03:06 +0300

    Remove app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { VotesModule } from './votes/votes.module';
 
 @Module({
   imports: [VotesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
