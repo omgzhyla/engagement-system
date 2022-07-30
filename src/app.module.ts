a8f53066f450cee79f0ab3dd6a0a946573a18038
Pavlo Zhyla <pavlo@pinpointestimates.com>
2022-07-30 13:38:46 +0300

    Empty Vote module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VotesModule } from './votes/votes.module';
 
 @Module({
-  imports: [],
+  imports: [VotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
